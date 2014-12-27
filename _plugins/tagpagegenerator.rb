d4b57618d8bd0b909beb01b5ae47f9e32d81e8e3
Ralf Gommers <ralf.gommers@googlemail.com>
2014-12-27 17:36:54 +0100

    MAINT: minor fix to tags usage.

--- _plugins/tagpagegenerator.rb
+++ _plugins/tagpagegenerator.rb
@@ -12,9 +12,6 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), site.config['tags']['layout'])
       self.data['tag'] = tag
-
-      tag_title_prefix = site.config['tags']['title_prefix'] || 'tag: '
-      self.data['title'] = "#{tag_title_prefix}#{tag}"
       self.data['filter_tag'] = "#{tag}"
     end
   end
